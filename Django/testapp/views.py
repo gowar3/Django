8f86b2283dfb8cff174a110cd144d2e5294011c0
gowar3 <110076117+gowar3@users.noreply.github.com>
2024-04-23 04:45:11 +0000

    Mon, Apr 22, 2024, 10:45 PM -06:00

--- Django/testapp/views.py
+++ Django/testapp/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world!")
+    return render(request, "testapp/index.html")
 
 def brian(request):
     return HttpResponse("Hello, Brian!")
