35058a1c52e6e0ea12c95be438d29395a98a9508
gowar3 <110076117+gowar3@users.noreply.github.com>
2024-04-23 04:23:05 +0000

    Mon, Apr 22, 2024, 10:23 PM -06:00

--- Django/Project1/urls.py
+++ Django/Project1/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("testapp/", include("testapp.urls"))
+    path("testapp/", include("testapp.urls")),
+    path("brian/", include("testapp.urls"))
 ]
