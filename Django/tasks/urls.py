22cc4d7ec9922313816a7fae4ef7bde1d153298d
gowar3 <110076117+gowar3@users.noreply.github.com>
2024-04-30 05:15:58 +0000

    Mon, Apr 29, 2024, 11:15 PM -06:00

--- Django/tasks/urls.py
+++ Django/tasks/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
-urlpatterns={
-    path("", views.index, name= "index")
-}
+app_name = "tasks"
+urlpatterns=[
+    path("", views.index, name= "index"),
+    path("add", views.add, name= "add")
+]
