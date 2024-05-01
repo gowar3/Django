3ec65252fbbf1b51418174abceded1a26f4025a2
gowar3 <110076117+gowar3@users.noreply.github.com>
2024-05-01 06:05:32 +0000

    Wed, May 1, 2024, 12:05 AM -06:00

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -11,6 +11,8 @@ def index(request):
 def search(request):
     result = util.get_entry(request)
 
+## Check why it does not return the result if found
+
     return render(request, "encyclopedia/search.html", {
         "result": result
     })
