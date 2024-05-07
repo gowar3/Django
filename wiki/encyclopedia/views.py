0ae662218bc373f64aded7849ac912703ed0b54b
gowar3 <110076117+gowar3@users.noreply.github.com>
2024-05-07 07:13:42 +0000

    Tue, May 7, 2024, 1:13 AM -06:00

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -49,7 +49,7 @@ def new(request):
 
         util.save_entry(request.POST["title"], request.POST["content"])
 
-        return HttpResponseRedirect(reverse("encylopedia/entry.html"), {
+        return HttpResponseRedirect(reverse("entry"), {
             "result": request.POST["title"]
         })
 
