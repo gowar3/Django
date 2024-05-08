d9e005fb96576df5e57bbb5a8b497ca73147e1ba
gowar3 <110076117+gowar3@users.noreply.github.com>
2024-05-08 06:16:31 +0000

    Wed, May 8, 2024, 12:16 AM -06:00

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -65,8 +65,9 @@ def rndm(request):
     x = random.randint(0, len(list) - 1)
 
 
-    #return HttpResponseRedirect(reverse("entry", args= [list[x]]))
 
     return render(request, "encyclopedia/entry.html", {
     "result": markdown2.markdown(util.get_entry(list[x]))
 })
+
+    #return HttpResponseRedirect(reverse("entry", args= [list[x]]))
