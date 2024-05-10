6cfdae87f9ef83516810347860907ad526b3d721
gowar3 <110076117+gowar3@users.noreply.github.com>
2024-05-10 05:01:07 +0000

    Thu, May 9, 2024, 11:01 PM -06:00

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path("search", views.search, name="search"),
     path("new", views.new, name="new"),
     path("random", views.rndm, name="random"),
-    path("edit", views.edit, name="edit"),
+    path("<str:edit>", views.edit, name="edit"),
     path("<str:entry>", views.entry, name="entry")
 ]
