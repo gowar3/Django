747257a49c8b81ced4d5a8ffb9ce5bff57028dba
gowar3 <110076117+gowar3@users.noreply.github.com>
2024-05-10 06:18:30 +0000

    Fri, May 10, 2024, 12:18 AM -06:00

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -36,7 +36,7 @@ def entry(request, entry):
 def search(request):
 
 #check how to search with cap insensitive
-    search_term = request.GET.get("q", "wiki/")
+    search_term = request.GET.get("q", "")
     result = util.get_entry(search_term)
 
 #check how to get the result with substring for search
