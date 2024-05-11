94d8599d9cc7782a835061c876091374e6bb7f33
gowar3 <110076117+gowar3@users.noreply.github.com>
2024-05-11 04:54:56 +0000

    Fri, May 10, 2024, 10:54 PM -06:00

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -42,7 +42,7 @@ def search(request):
 
     for entry in entries:
 
-        if search_term.lower() in entry.lower():
+        if search_term in entry.lower():
 
             result = util.get_entry(entry)
 
