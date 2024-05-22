0a88b75aa09049a38f164bd7b4408d65ea7680dd
gowar3 <110076117+gowar3@users.noreply.github.com>
2024-05-22 04:12:03 +0000

    Tue, May 21, 2024, 10:12 PM -06:00

--- airline/flights/models.py
+++ airline/flights/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-class Flight(models.model):
+class Flight(models.Model):
 
     origin = models.CharField(max_length = 64)
     destination = models.CharField(max_length = 64)
