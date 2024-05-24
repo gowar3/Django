4ac10eb0764081ab0aa97b4193f592c6ecb83154
gowar3 <110076117+gowar3@users.noreply.github.com>
2024-05-24 05:17:11 +0000

    Thu, May 23, 2024, 11:17 PM -06:00

--- airline/flights/views.py
+++ airline/flights/views.py
@@ -8,5 +8,5 @@ from .models import Flight
 def index(request):
 
     return render(request, "flights/index.html", {
-        "flights" = Flight.objects.all()
+        "flights": Flight.objects.all()
     })
