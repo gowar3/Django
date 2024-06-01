ee614a1db87b9025423d312a12f0ed38f71c5929
gowar3 <110076117+gowar3@users.noreply.github.com>
2024-06-01 04:15:42 +0000

    Fri, May 31, 2024, 10:15 PM -06:00

--- airline/flights/admin.py
+++ airline/flights/admin.py
@@ -5,8 +5,8 @@ from .models import Flight, Airport, Passenger
 # Register your models here.
 
 class FlightAdmin(admin.ModelAdmin):
-    
+    list_display = ("id", "origin", "destination", "duration")
 
 admin.site.register(Airport)
-admin.site.register(Flight)
+admin.site.register(Flight, FlightAdmin)
 admin.site.register(Passenger)
