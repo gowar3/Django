845e11a43dd28165867b2d5419b803aa262299de
gowar3 <110076117+gowar3@users.noreply.github.com>
2024-06-01 04:55:07 +0000

    Fri, May 31, 2024, 10:55 PM -06:00

--- airline/users/urls.py
+++ airline/users/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from . import views
 
-urlspatterns = [
+urlpatterns = [
     path("", views.index, name="index"),
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout")
