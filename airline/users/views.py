acc84d7a05bab69b04e61cf69d68374c582197a1
gowar3 <110076117+gowar3@users.noreply.github.com>
2024-06-01 05:08:19 +0000

    Fri, May 31, 2024, 11:08 PM -06:00

--- airline/users/views.py
+++ airline/users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 
@@ -11,6 +12,20 @@ def index(request):
         return HttpResponseRedirect(reverse("login"))
 
 def login_view(request):
+
+    if request.method=="POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "users/login.html", {
+                "message": "Invalid Credentials."
+            })
 
     return render(request, "users/login.html")
 
