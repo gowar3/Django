1782c509d65344e1e3fdb0dfd70c6eb2302d91ab
gowar3 <110076117+gowar3@users.noreply.github.com>
2024-06-05 05:42:15 +0000

    Tue, Jun 4, 2024, 11:42 PM -06:00

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -4,3 +4,15 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Listing(models.Model):
+
+    
+
+    origin = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="departures")
+    destination = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="arrivals")
+    duration = models.IntegerField()
+
+    def __str__(self):
+
+        return f"{self.id}: {self.origin} to {self.destination}"
