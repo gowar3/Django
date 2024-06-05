abec633cab06dace14f752776e6f941f7532ebff
gowar3 <110076117+gowar3@users.noreply.github.com>
2024-06-05 05:45:47 +0000

    Tue, Jun 4, 2024, 11:45 PM -06:00

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -10,11 +10,6 @@ class Listing(models.Model):
     Name = models.CharField(max_length=64)
     Price = models.IntegerField()
 
-
-    origin = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="departures")
-    destination = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="arrivals")
-    duration = models.IntegerField()
-
     def __str__(self):
 
         return f"{self.id}: {self.origin} to {self.destination}"
