f9bc4ba26ddb6ca2b82bb537d66f445c9d88efcb
gowar3 <110076117+gowar3@users.noreply.github.com>
2024-06-11 06:02:18 +0000

    Tue, Jun 11, 2024, 12:02 AM -06:00

--- commerce/auctions/views.py
+++ commerce/auctions/views.py
@@ -93,7 +93,7 @@ def listing(request, listing):
 
 
 
-def wishlist(request, wishlist):
+def wishlist(request, wish):
 
     if "wishlist" not in request.session:
 
@@ -104,7 +104,7 @@ def wishlist(request, wishlist):
     if request.method == "POST":
 
 
-        wish = Listing.objects.get(title = wishlist)
+        wish = Listing.objects.get(title = wish)
 
         request.session["wishlist"] += [wish]
 
