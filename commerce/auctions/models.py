d56fba719594e02f831bf2cb48cdd3617de3eae5
gowar3 <110076117+gowar3@users.noreply.github.com>
2024-06-13 04:10:26 +0000

    Wed, Jun 12, 2024, 10:10 PM -06:00

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -26,5 +26,9 @@ class Bid(models.Model):
 
 class Comment(models.Model):
 
-    user = models.CharField(max_length=128)
-    
+    user = models.CharField(max_length=64)
+    comments = models.CharField(max_lenght=128)
+
+    def __str__(self):
+
+        return f"{self.user}: {self.comments}"
