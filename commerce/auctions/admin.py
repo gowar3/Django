1999f8aaddb99f686c42e3681024029828fbd321
gowar3 <110076117+gowar3@users.noreply.github.com>
2024-06-13 04:14:39 +0000

    Wed, Jun 12, 2024, 10:14 PM -06:00

--- commerce/auctions/admin.py
+++ commerce/auctions/admin.py
@@ -1,11 +1,19 @@
 from django.contrib import admin
 
-from .models import Listing
+from .models import Listing, Bid, Comment
 
 # Register your models here.
 
 class ListingAdmin(admin.ModelAdmin):
     list_display = ("id", "title", "description", "price")
 
+class BidAdmin(admin.ModelAdmin):
+    list_display = ("owner", "offer")
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("user", "comments")
+
 
 admin.site.register(Listing, ListingAdmin)
+admin.site.register(Bid, BidAdmin)
+admin.site.register(Comment, CommentAdmin)
