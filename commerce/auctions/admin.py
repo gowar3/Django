069197bbdb08e0955180a1372e5ee974fda97689
gowar3 <110076117+gowar3@users.noreply.github.com>
2024-06-13 05:15:08 +0000

    Wed, Jun 12, 2024, 11:15 PM -06:00

--- commerce/auctions/admin.py
+++ commerce/auctions/admin.py
@@ -11,7 +11,7 @@ class BidAdmin(admin.ModelAdmin):
     list_display = ("owner", "offer")
 
 class CommentAdmin(admin.ModelAdmin):
-    list_display = ("user", "comments")
+    list_display = ("user", "comment")
 
 
 admin.site.register(Listing, ListingAdmin)
