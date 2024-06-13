920890a50a5d884c246b2f88df45bb02a3cb2d8a
gowar3 <110076117+gowar3@users.noreply.github.com>
2024-06-13 06:10:39 +0000

    Thu, Jun 13, 2024, 12:10 AM -06:00

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -10,7 +10,7 @@ class Listing(models.Model):
     title = models.CharField(max_length=64)
     description = models.CharField(max_length=64)
     price = models.IntegerField()
-    comments = models.ManyToManyField(Comment, blank=True, related_name="listings")
+    comments = models.ManyToManyField(Comment, blank=True, related_name="listing")
 
     def __str__(self):
 
