a6414ddab13b22a3907f9b67d7d911f2b7e54902
gowar3 <110076117+gowar3@users.noreply.github.com>
2024-06-14 05:50:15 +0000

    Thu, Jun 13, 2024, 11:50 PM -06:00

--- commerce/auctions/admin.py
+++ commerce/auctions/admin.py
@@ -5,7 +5,7 @@ from .models import Listing, Bid, Comment
 # Register your models here.
 
 class ListingAdmin(admin.ModelAdmin):
-    list_display = ("id", "user", "title", "description", "price")
+    list_display = ("id", "title", "description", "price")
 
 class BidAdmin(admin.ModelAdmin):
     list_display = ("owner", "offer")
