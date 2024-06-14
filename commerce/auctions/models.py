8305670c74cb1cc061931b554726eaa69f9e39ca
gowar3 <110076117+gowar3@users.noreply.github.com>
2024-06-14 05:05:22 +0000

    Thu, Jun 13, 2024, 11:05 PM -06:00

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -8,6 +8,7 @@ class User(AbstractUser):
 
 class Listing(models.Model):
 
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=64)
     description = models.CharField(max_length=64)
     price = models.IntegerField()
