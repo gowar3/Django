40d6ccf871329fed70abb14f683159f0451b34bf
gowar3 <110076117+gowar3@users.noreply.github.com>
2024-06-15 04:49:52 +0000

    Fri, Jun 14, 2024, 10:49 PM -06:00

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -19,7 +19,7 @@ class Listing(models.Model):
 
 class Comment(models.Model):
 
-    poster = models.CharField(max_length=64)
+    poster = models.CharField(max_length=64, default="Anonymous")
     comment = models.CharField(max_length=128)
     listings = models.ManyToManyField(Listing, blank=True, related_name= "comments")
 
