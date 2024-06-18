e1b3a43eb95ff5d1e0802c81db2a3a3655a21332
gowar3 <110076117+gowar3@users.noreply.github.com>
2024-06-18 06:08:31 +0000

    Tue, Jun 18, 2024, 12:08 AM -06:00

--- commerce/auctions/views.py
+++ commerce/auctions/views.py
@@ -158,7 +158,7 @@ def wishlist(request, user):
 
 
     return render(request, "auctions/wishlist.html", {
-        "wishlist": wish.users.all()
+        "wishlist": user.wishlist.all()
     })
 
 
