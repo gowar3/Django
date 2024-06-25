5280ba6b8070545b0e9e9ea277a5f45ce7593a03
gowar3 <110076117+gowar3@users.noreply.github.com>
2024-06-25 05:12:55 +0000

    Mon, Jun 24, 2024, 11:12 PM -06:00

--- commerce/auctions/views.py
+++ commerce/auctions/views.py
@@ -99,8 +99,8 @@ def listing(request, listing):
     error = ""
     listing = Listing.objects.get(title = listing)
     username = request.user.username
-    highest_bid = listing.bids.aggregate(max_offer=Max("offer"))
-    highest_bid_value = highest_bid.get("max_offer", None)
+    highest_bid_value = 0
+
 
     if request.method == "POST":
 
@@ -121,6 +121,8 @@ def listing(request, listing):
 
         if bid != "" and int(bid) > listing.price and int(bid) > highest_bid_value:
 
+            highest_bid = listing.bids.aggregate(max_offer=Max("offer"))
+            highest_bid_value = highest_bid.get("max_offer")
 
             new_bid = Bid.objects.create(owner=username, offer=bid, listing=listing)
 
