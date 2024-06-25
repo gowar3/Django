613a490f286560d90b80ee1901d22e18a13a200e
gowar3 <110076117+gowar3@users.noreply.github.com>
2024-06-25 04:56:05 +0000

    Mon, Jun 24, 2024, 10:56 PM -06:00

--- commerce/auctions/views.py
+++ commerce/auctions/views.py
@@ -98,6 +98,7 @@ def listing(request, listing):
     error = ""
     listing = Listing.objects.get(title = listing)
     username = request.user.username
+    highest_bid = Listing.bids.aggregate(max_offer=Max("offer"))
 
     if request.method == "POST":
 
