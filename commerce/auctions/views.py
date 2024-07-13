c5fe52f71acec349e561a24cb6ebacc1d1c1e942
gowar3 <110076117+gowar3@users.noreply.github.com>
2024-07-13 05:58:31 +0000

    Fri, Jul 12, 2024, 11:58 PM -06:00

--- commerce/auctions/views.py
+++ commerce/auctions/views.py
@@ -209,7 +209,7 @@ def category(request, category):
 
 
     type = Category.objects.get(name=category)
-    listings = type.categories.all()
+    listings = type.listing_set.all()
 
     return render(request, "auctions/category.html", {
         "category": category,
