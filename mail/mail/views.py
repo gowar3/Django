57708bf8eabadcf18a6ecf63fb162b437cac3f73
gowar3 <110076117+gowar3@users.noreply.github.com>
2024-07-17 05:18:24 +0000

    Tue, Jul 16, 2024, 11:18 PM -06:00

--- mail/mail/views.py
+++ mail/mail/views.py
@@ -166,7 +166,7 @@ def register(request):
 
         # Attempt to create new user
         try:
-            user = User.objects.create_user(email, password, password)
+            user = User.objects.create_user(email, email, password)
             user.save()
         except IntegrityError as e:
             print(e)
