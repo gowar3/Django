e5ea9355e7f175812e575090f972898192cecf35
gowar3 <110076117+gowar3@users.noreply.github.com>
2025-11-18 02:47:47 +0000

    change

--- mail/mail/static/mail/inbox.js
+++ mail/mail/static/mail/inbox.js
@@ -106,7 +106,7 @@ document.addEventListener('DOMContentLoaded', function() {
     document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
 
 
-    fetch('/emails/<str:mailbox>')
+    fetch(`/emails/${mailbox}`)
 
     .then(response => response.json())
 
