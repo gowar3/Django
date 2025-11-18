423f3e85bf5c95033c363392f491ce2fcd69c30e
gowar3 <110076117+gowar3@users.noreply.github.com>
2025-11-18 05:13:24 +0000

    fkfkfkg

--- mail/mail/static/mail/inbox.js
+++ mail/mail/static/mail/inbox.js
@@ -101,6 +101,7 @@ document.addEventListener('DOMContentLoaded', function() {
     // Show the mailbox and hide other views
     document.querySelector('#emails-view').style.display = 'block';
     document.querySelector('#compose-view').style.display = 'none';
+    document.querySelector('#email-view').style.display = 'none';
 
     // Show the mailbox name
 
